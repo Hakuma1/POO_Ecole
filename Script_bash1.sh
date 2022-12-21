937e917f91e7b471fe0ad7667012ad7054eeb830
mac <mac@iMac-de-mac.local>
2022-12-21 13:16:29 +0100

    Ajout des scripts Script_bash1.sh et Script_bash2.sh

---
+++ Script_bash1.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+touch /home/mamady/Documents/testCRON/test
+
+#Code qui permet la création d'un fichier
+
+#Afin de le lancer chaque 6H, on fait ceci:
+#* */6 * * * /home/mamady/Documents/test1.sh
---
+++ Script_bash2.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+touch /home/mamady/Documents/testCRON/test
+
+#Code qui permet la création d'un fichier
+
+#Afin de le lancer à chaque démarage, on fait ceci:
+#@reboot /home/mamady/Documents/test1.sh
